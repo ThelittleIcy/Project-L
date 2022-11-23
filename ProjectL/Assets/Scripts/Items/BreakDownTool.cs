dba65003ce120b1fd8bf6c381aa1bb8e4273bad6
Jessica Artz <JessicaArtz01@gmail.com>
2022-11-23 16:53:32 +0100

    Added Using the Items, changed the Tool structure

--- ProjectL/Assets/Scripts/Items/BreakDownTool.cs
+++ ProjectL/Assets/Scripts/Items/BreakDownTool.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class Axe : AItem
+
+public class BreakDownTool : AItem
 {
     public Vector2 UseDirection { get => m_useDirection; set => m_useDirection = value; }
     public Transform OriginPlayer { get => m_originPlayer; set => m_originPlayer = value; }
@@ -12,26 +13,38 @@ public class Axe : AItem
     private float m_range = 2f;
     [SerializeField]
     private Transform m_originPlayer;
+    [SerializeField]
+    private LayerMask m_useLayerMask;
+    [SerializeField]
+    private Tools m_type;
+
     public override void AddToInventory()
     {
         base.AddToInventory();
-        PlayerTools.Instance.Add(this);
+        PlayerTools.Instance.Add(this, m_type);
     }
     public override void RemoveFromInventory()
     {
         base.RemoveFromInventory();
-        PlayerTools.Instance.Remove(this);
+        PlayerTools.Instance.Remove(this, m_type);
     }
 
     public override void Use()
     {
         base.Use();
-        RaycastHit2D hit = Physics2D.Raycast(new Vector2(OriginPlayer.position.x, OriginPlayer.position.y), UseDirection, m_range);
-        if (hit.collider.gameObject.GetComponent(typeof(IDegradable)))
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(OriginPlayer.position.x, OriginPlayer.position.y), UseDirection, m_range, m_useLayerMask);
+        if(hit == false)
         {
-
+            return;
         }
-        Debug.Log("Used the Axe");
+        if (hit.collider.gameObject.GetComponent(typeof(IFarmable)) != null)
+        {
+            if (hit.collider.gameObject.GetComponent<FarmRessource>().NeededTool == m_type)
+            {
+                hit.collider.gameObject.GetComponent<FarmRessource>().Farm();
+            }
+        }
+        Debug.Log("Used the Tool");
     }
     [ContextMenu("Test")]
     public override void Select()
--- ProjectL/Assets/Scripts/Player/PlayerTools.cs
+++ ProjectL/Assets/Scripts/Player/PlayerTools.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class PlayerTools : MonoBehaviour
 {
-    public Shovel Shovel { get => m_shovel; set => m_shovel = value; }
-    public Pickaxe Pickaxe { get => m_pickaxe; set => m_pickaxe = value; }
-    public Axe Axe { get => m_axe; set => m_axe = value; }
+    public BreakDownTool Shovel { get => m_shovel; set => m_shovel = value; }
+    public BreakDownTool Pickaxe { get => m_pickaxe; set => m_pickaxe = value; }
+    public BreakDownTool Axe { get => m_axe; set => m_axe = value; }
     public static PlayerTools Instance { get => m_instance; set => m_instance = value; }
 
     [SerializeField]
-    private Axe m_axe;
+    private BreakDownTool m_axe;
     [SerializeField]
-    private Pickaxe m_pickaxe;
+    private BreakDownTool m_pickaxe;
     [SerializeField]
-    private Shovel m_shovel;
+    private BreakDownTool m_shovel;
     [SerializeField]
     private PlayerMovement m_player;
     [SerializeField]
@@ -79,52 +79,67 @@ public class PlayerTools : MonoBehaviour
             m_currentlySelected.Use();
         }
     }
-    public void Add(Axe _new)
+    public void Add(BreakDownTool _new, Tools _type)
     {
-        if (m_axe != null)
+        switch (_type)
         {
-            m_axe.Drop();
-        }
-        m_axe = _new;
-    }
-    public void Add(Pickaxe _new)
-    {
-        if (m_pickaxe != null)
-        {
-            m_pickaxe.Drop();
-        }
-        m_pickaxe = _new;
-    }
-    public void Add(Shovel _new)
-    {
-        if (m_shovel != null)
-        {
-            m_shovel.Drop();
-        }
-        m_shovel = _new;
-    }
-    public void Remove(Axe _toRemove)
-    {
-        if (m_axe == _toRemove)
-        {
-            m_axe = null;
-            m_currentlySelected = null;
-        }
-    }
-    public void Remove(Pickaxe _toRemove)
-    {
-        if (m_pickaxe == _toRemove)
-        {
-            m_pickaxe = null;
-            m_currentlySelected = null;
+            case Tools.NONE:
+                break;
+            case Tools.AXE:
+                if (m_axe != null)
+                {
+                    m_axe.Drop();
+                }
+                m_axe = _new;
+                break;
+            case Tools.PICKAXE:
+                if (m_pickaxe != null)
+                {
+                    m_pickaxe.Drop();
+                }
+                m_pickaxe = _new;
+                break;
+            case Tools.SHOVEL:
+                if (m_shovel != null)
+                {
+                    m_shovel.Drop();
+                }
+                m_shovel = _new;
+                break;
+            default:
+                break;
         }
     }
-    public void Remove(Shovel _toRemove)
+
+    public void Remove(BreakDownTool _toRemove, Tools _type)
     {
-        if (m_shovel == _toRemove)
+        switch (_type)
         {
-            m_shovel = null;
-            m_currentlySelected = null;
+            case Tools.NONE:
+                break;
+            case Tools.AXE:
+                if (m_axe == _toRemove)
+                {
+                    m_axe = null;
+                    m_currentlySelected = null;
+                }
+                break;
+            case Tools.PICKAXE:
+                if (m_pickaxe == _toRemove)
+                {
+                    m_pickaxe = null;
+                    m_currentlySelected = null;
+                }
+                break;
+            case Tools.SHOVEL:
+                if (m_shovel == _toRemove)
+                {
+                    m_shovel = null;
+                    m_currentlySelected = null;
+                }
+                break;
+            default:
+                break;
         }
     }
 }
