ceb2fe56074155b78f0137fe4dab1e1b9a3faa05
Jessica Artz <JessicaArtz01@gmail.com>
2023-01-02 23:20:43 +0100

    started Working On VisionSense - Field of View

--- ProjectL/Assets/Scripts/AI/Senses/VisionSense.cs
+++ ProjectL/Assets/Scripts/AI/Senses/VisionSense.cs
@@ -9,6 +9,17 @@ public class VisionSense : ASense
     private Transform m_player;
     [SerializeField]
     private EnemyStats m_stats;
+
+    [SerializeField]
+    private float m_radius = 5f;
+    [SerializeField]
+    [Range(1,360)]
+    private float m_angle = 45f;
+
+    [SerializeField]
+    private LayerMask m_targetLayer;
+    [SerializeField]
+    private LayerMask m_obstructionLayer;
     /// <summary>
     /// Sets Up the Seeing Dictionary - In Vision, Blind etc.
     /// </summary>
@@ -33,6 +44,23 @@ public class VisionSense : ASense
 
     private bool IsInVision()
     {
+        Collider2D[] rangeCheck = Physics2D.OverlapCircleAll(transform.position, m_radius, m_targetLayer);
+
+        if(rangeCheck.Length > 0)
+        {
+            Transform target = rangeCheck[0].transform;
+            Vector2 directionToTarget = (target.position - transform.position).normalized;
+
+            if(Vector2.Angle(transform.up, directionToTarget) < m_angle * 0.5f)
+            {
+                float distanceToTarget = Vector2.Distance(transform.position, target.position);
+
+                if(!Physics2D.Raycast(transform.position, directionToTarget, distanceToTarget, m_obstructionLayer))
+                {
+                    return true;
+                }
+            }
+        }
         return false;
     }
     private bool IsBlind()
@@ -42,5 +70,22 @@ public class VisionSense : ASense
             return true;
         }
         return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.forward, m_radius);
+
+        Vector3 angle1 = DirectionFromAngles(-transform.eulerAngles.z, -m_angle * 0.5f);
+        Vector3 angle2 = DirectionFromAngles(-transform.eulerAngles.z, m_angle * 0.5f);
+
+        Gizmos.DrawLine(transform.position, transform.position + angle1 * m_radius);
+        Gizmos.DrawLine(transform.position, transform.position + angle2 * m_radius);
+    }
+    private Vector2 DirectionFromAngles(float _eulerY, float _angleDegrees)
+    {
+        _angleDegrees += _eulerY;
+        return new Vector2(Mathf.Sin(_angleDegrees * Mathf.Deg2Rad), Mathf.Cos(_angleDegrees * Mathf.Deg2Rad));
     }
 }
